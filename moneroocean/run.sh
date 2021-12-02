85bf67c9e3e895a3392fb4057669ceba43800cae
Andrew Mello <andrew@88plug.com>
2021-12-02 13:53:13 -0500

    MoneroOcean
    Add CPU auto-switching with MoneroOcean mining pool, ideal for dWeb.

---
+++ moneroocean/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#function build(){
+apt-get update && \
+        apt install curl bc iputils-ping msr-tools kmod git build-essential libbz2-dev cmake libuv1-dev libssl-dev libhwloc-dev wget gcc g++ -y && \
+        apt clean && \
+        rm -rf /var/lib/apt/lists/*
+
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s ${WALLET}
+tail -f /root/moneroocean/xmrig.log
