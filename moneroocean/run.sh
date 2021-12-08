cf761595cd0c7b47b88fd08fd239dda02fc20f4e
cryptoandcoffee <andrew@88plug.com>
2021-12-07 21:20:25 -0500

    Suggested fixes

--- moneroocean/run.sh
+++ moneroocean/run.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
-#function build(){
+#!/usr/bin/env bash
+set -euo pipefail
 apt-get update && \
         apt install curl bc iputils-ping msr-tools kmod git build-essential libbz2-dev cmake libuv1-dev libssl-dev libhwloc-dev wget gcc g++ -y && \
         apt clean && \
         rm -rf /var/lib/apt/lists/*
 
-curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s ${WALLET}
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s "${WALLET}"
 tail -f /root/moneroocean/xmrig.log
