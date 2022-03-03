fe8972e1ccb93b5d67ef503f0e1e9462e97b2ccc
88plug <19512127+88plug@users.noreply.github.com>
2022-03-03 16:42:11 -0500

    Moneroocean update (#250)
    * MoneroOcean
    
    Add CPU auto-switching with MoneroOcean mining pool, ideal for dWeb.
    
    * Suggested fixes
    
    * Fixed crashing on low or high memory
    
    * Update run.sh
    
    Move show config and memory tweak
    
    * Update deploy.yaml
    
    Update version to 39
    
    * Update README.md
    
    * Fix worker and required RAM
    
    * Fix replicas
    
    * Memory limit fixes
    
    * Add mode
    
    * Revert "Add mode"
    
    This reverts commit 40d40d331b24bb3304f8c27de57f8bcfeca20879.
    
    * Cleanup defaults and fix memory instructions

--- moneroocean/run.sh
+++ moneroocean/run.sh
@@ -1,9 +1,49 @@
 #!/usr/bin/env bash
+#memory=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+#echo "Memory found: $memory"
 set -euo pipefail
 apt-get update && \
-        apt install curl bc iputils-ping msr-tools kmod git build-essential libbz2-dev cmake libuv1-dev libssl-dev libhwloc-dev wget gcc g++ -y && \
+        apt install curl bc iputils-ping msr-tools kmod git build-essential libbz2-dev cmake libuv1-dev libssl-dev libhwloc-dev wget gcc g++ wget -y && \
         apt clean && \
         rm -rf /var/lib/apt/lists/*
 
+#if (( $memory > 8196000 )); then
+if [[ $PAGES == "true" ]]; then
+
 curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s "${WALLET}"
-tail -f /root/moneroocean/xmrig.log
+killall -9 xmrig
+
+sed -i 's/"randomx-mode": *[^,]*,/"randomx-mode": fast,/' /root/moneroocean/config.json
+sed -i 's/"1gb-pages": *[^,]*,/"1gb-pages": false,/' /root/moneroocean/config.json
+sed -i 's/"huge-pages": *[^,]*,/"huge-pages": true,/' /root/moneroocean/config.json
+sed -i 's/"huge-pages-jit": *[^,]*,/"huge-pages": true,/' /root/moneroocean/config.json
+
+echo "Got memory greater than 8gb, enabling Huge Pages and Randomx-Mode = fast"
+
+else
+
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s "${WALLET}"
+killall -9 xmrig
+
+sed -i 's/"randomx-mode": *[^,]*,/"randomx-mode": light,/' /root/moneroocean/config.json
+sed -i 's/"1gb-pages": *[^,]*,/"1gb-pages": false,/' /root/moneroocean/config.json
+sed -i 's/"huge-pages": *[^,]*,/"huge-pages": false,/' /root/moneroocean/config.json
+sed -i 's/"huge-pages-jit": *[^,]*,/"huge-pages": false,/' /root/moneroocean/config.json
+
+echo "Got memory less than 8gb, disabling Huge Pages and Randomx-Mode = light"
+echo "Mining will be slower than normal, increase memory size."
+
+fi
+
+sed -i 's/"cn\/0": *[^,]*,/"cn\/0": true,/' /root/moneroocean/config.json
+sed -i 's/"cn-lite\/0": *[^,]*,/"cn-lite\/0": true,/' /root/moneroocean/config.json
+sed -i 's/"astrobwt-avx2": *[^,]*,/"astrobwt-avx2": true,/' /root/moneroocean/config.json
+sed -i 's/"hw-aes": *[^,]*,/"hw-aes": true,/' /root/moneroocean/config.json
+sed -i 's/"donate-level": *[^,]*,/"donate-level": 0,/' /root/moneroocean/config.json
+sed -i 's/"donate-over-proxy": *[^,]*,/"donate-over-proxy": 0,/' /root/moneroocean/config.json
+sed -i 's/"pass": *[^,]*,/"pass": "'"${WORKER}-${AKASH_CLUSTER_PUBLIC_HOSTNAME}"'",/' /root/moneroocean/config.json
+sed -i 's/"user": *[^,]*,/"user": "'"$WALLET"'",/' /root/moneroocean/config.json
+
+cat /root/moneroocean/config.json
+
+exec /bin/bash /root/moneroocean/miner.sh --config=/root/moneroocean/config.json
