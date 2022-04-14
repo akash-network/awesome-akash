521b5c8882ac07349f156c4540e3ddae098f64cc
cryptoandcoffee <andrew@88plug.com>
2022-04-14 14:23:51 -0400

    Fix up indentation

--- chia/sync.sh
+++ chia/sync.sh
@@ -1,23 +1,22 @@
 echo "Check this every 15 seconds"
-for (( ; ; ))
-do
+for (( ; ; )); do
 
-files=$(ls -la /plots/*.plot | awk '{print $9}')
-count=$(ls -la /plots/*.plot | wc -l)
+	files=$(ls -la /plots/*.plot | awk '{print $9}')
+	count=$(ls -la /plots/*.plot | wc -l)
 
-echo "Found $count files to sync"
+	echo "Found $count files to sync"
 
-pending=$(cat /plots/pending.log)
+	pending=$(cat /plots/pending.log)
 
-for i in $files; do
+	for i in $files; do
 
-if grep -q $i /plots/pending.log
-then
- echo "Skipping $i - alreading uploading!"
-else
- nohup sshpass -e rsync -av --remove-source-files --progress $i -e "ssh -p ${REMOTE_PORT}" "${REMOTE_USER}"@"${REMOTE_HOST}":"${REMOTE_LOCATION}" >> /plots/rsync.log 2>&1 &
- echo $i >> /plots/pending.log
-fi
-done
-sleep 15
+		if grep -q $i /plots/pending.log; then
+			echo "Skipping $i - alreading uploading!"
+		else
+			nohup sshpass -e rsync -av --remove-source-files --progress $i -e "ssh -p ${REMOTE_PORT}" "${REMOTE_USER}"@"${REMOTE_HOST}":"${REMOTE_LOCATION}" >>/plots/rsync.log 2>&1 &
+			echo $i >>/plots/pending.log
+		fi
+	done
+
+	sleep 15
 done
