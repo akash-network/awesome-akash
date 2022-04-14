2d4fc4ab5608ca66abc802aab14b477f8e8a7bc8
88plug <19512127+88plug@users.noreply.github.com>
2022-04-14 15:34:40 -0400

    Chia Plotting Fixes (#285)
    * Add REMOTE_LOCATION= variable
    
    * Fix up quotes
    
    * Bump version
    
    * Update to new version
    
    * Bump version
    
    * Add support for local plotting and download links
    
    * Add file manager to REMOTE_LOCATION=local
    
    * Add delete
    
    * Add bladebit tested with persistent stoage
    
    * Bigger limits
    
    * Thread count fix
    
    * Thread count
    
    * Use default 10 threads if none specified
    
    * Cleanup documentation
    
    * Update log instructions
    
    * Fix attribute usage
    
    * -Fixes madmax error with TMPDIR not being set properly
    -Uses imagePullPolicy: Always now in deployment manifest to ensure latest version, no more version numbers required.
    
    * Remove imagePullPolicy from SDL
    
    * Add signedBy and version
    
    * Update README
    
    * Max 7 plots
    
    * Fix ssh check on upload and limit max plots
    
    SSH Upload check fixed
    Max 7 plots in 1Tb container
    
    * Fixes runtime error with python3.9
    
    * Upload background
    
    * Multi threaded uploading now enabled
    
    * Upload background and new deployment size
    
    * Fix up indentation
    
    Co-authored-by: Crypto and Coffee <null@cryptoandcoffee.com>

--- chia/run.sh
+++ chia/run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-#mkdir tmp 
-#for CA certificates
+
+#SSH Test if required
 if [[ "$REMOTE_LOCATION" != "local" ]]; then
-echo "Upload test before unpacking..."
-mkdir -p /root/.ssh/
-touch /root/.ssh/known_hosts
-echo "Found $REMOTE_HOST with user $REMOTE_USER on port $REMOTE_PORT to upload to"
-ssh-keyscan -p "${REMOTE_PORT}" "${REMOTE_HOST}" >> ~/.ssh/known_hosts
-ssh -o BatchMode=yes -o ConnectTimeout=5 -p "$REMOTE_PORT" "$REMOTE_USER"@"$REMOTE_HOST" echo Upload Destination Connection OK 2>&1 || echo "Did not pass go! Check your SSH settings" && exit 1
-else
+	echo "SSH connection test before unpacking..."
+	mkdir -p /root/.ssh/
+	touch /root/.ssh/known_hosts
+	echo "Found $REMOTE_HOST with user $REMOTE_USER on port $REMOTE_PORT to upload to"
+	ssh-keyscan -p "${REMOTE_PORT}" "${REMOTE_HOST}" >>~/.ssh/known_hosts
+	export SSHPASS=${REMOTE_PASS}
+
+	sshpass -e ssh -o ConnectTimeout=5 -p $REMOTE_PORT $REMOTE_USER@$REMOTE_HOST exit
+	if [ $? != 0 ]; then
+		echo "SSH Connection failed - please check your settings"
+		exit
+	fi
+fi
+
 mkdir /plots
 chmod 777 /plots -R
 git clone https://github.com/prasathmani/tinyfilemanager /filemanager
@@ -29,27 +36,42 @@ sed -i -e "s/Tiny Chia Plot Manager/Chia Plot Manager/g" /plots/index.php
 /etc/init.d/nginx start
 /etc/init.d/php8.1-fpm start
 
-
-echo "###################################################################################################"
-echo "###################################################################################################"
-echo "###################################################################################################"
-echo "###################################################################################################"
-echo "###################################################################################################"
-echo "Plots will be created locally.  Please check Akashlytics for the Uri - you can find this on the"
-echo "deployment details page.  Plots will only appear after creation.  Please be patient for your first"
-echo "plots to appear.  Sleeping 60 seconds before starting..."
-echo "###################################################################################################"
-echo "###################################################################################################"
-echo "###################################################################################################"
-echo "###################################################################################################"
-echo "###################################################################################################"
-
-sleep 60
+if [[ "$REMOTE_LOCATION" == "local" ]]; then
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "Plots will be created locally.  Please check Akashlytics for the Uri - you can find this on the"
+	echo "deployment details page.  Plots will only appear after creation.  Please be patient for your first"
+	echo "plots to appear.  Sleeping 60 seconds before starting..."
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	sleep 60
+else
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "Plots will be uploaded to $REMOTE_LOCATION on $REMOTE_HOST.                                        "
+	echo "After the plot is succesfully uploaded it will be deleted automatically from the deployment        "
+	echo "Sleeping 10 seconds before starting...                                                             "
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	echo "###################################################################################################"
+	sleep 10
 fi
+
 echo "Let's get thing started..."
 ram=$(free -m | grep -oP '\d+' | head -n 1)
 threads=$(grep -c ^processor /proc/cpuinfo)
-cores=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}' )
+cores=$(grep ^cpu\\scores /proc/cpuinfo | uniq | awk '{print $4}')
 
 echo "Found RAM: $ram"
 echo "Found THREADS: $threads"
@@ -58,47 +80,66 @@ echo "Found CORES: $cores"
 #Max out threads
 echo Using $THREADS as defined by user
 
-mkdir -p /root/chia/final ; mkdir -p /root/chia/tmp2 ; mkdir -p /root/chia/tmp
+mkdir -p /root/chia/final
+mkdir -p /root/chia/tmp2
+mkdir -p /root/chia/tmp
+
+if [[ "$UPLOAD_BACKGROUND" == "true" ]]; then
+	screen -dmS sync bash ./sync.sh
+fi
 
 rm -rf chia-blockchain
-git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules ; cd chia-blockchain
-sh install.sh ; . ./activate
+git clone https://github.com/Chia-Network/chia-blockchain.git -b latest --recurse-submodules
+cd chia-blockchain
+sh install.sh
+. ./activate
 chia init
 
 if [ ! -z $KEYS ]; then
-echo "Foud KEYS variable set, importing"
-echo ${KEYS} > keys.txt ; chia keys add -f ./keys.txt ; rm keys.txt ; chia keys show
+	echo "Foud KEYS variable set, importing"
+	echo ${KEYS} >keys.txt
+	chia keys add -f ./keys.txt
+	rm keys.txt
+	chia keys show
 fi
 
 if [ ! -z $PLOTTER ]; then
-while :
-do
-chmod 777 /plots -R
-if [[ "$REMOTE_LOCATION" != "local" ]]; then
+	while :; do
+		chmod 777 /plots -R
 
-sshpass -p "${REMOTE_PASS}" rsync -av --remove-source-files --progress /root/chia/final/*.plot -e "ssh -p ${REMOTE_PORT}" "${REMOTE_USER}"@"${REMOTE_HOST}":"${REMOTE_LOCATION}"
-if [[ ${PLOTTER} == "madmax" ]]; then
-chia plotters madmax -k $SIZE -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -2 $TMPDIR2 -d $FINALDIR
-elif [[ ${PLOTTER} == "blade" ]]; then
-apt-get install -y libgmp3-dev
-chia plotters bladebit -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -d $FINALDIR
-else
-chia plots create -k $SIZE -n $COUNT -r $THREADS -b $MEMORY -c $CONTRACT -f $FARMERKEY -t $TMPDIR -2 $TMPDIR2 -d $FINALDIR
-fi
+		if [[ "$REMOTE_LOCATION" != "local" ]]; then
 
-else
+			if [[ "$UPLOAD_BACKGROUND" == "false" ]]; then
+				sshpass -e rsync -av --remove-source-files --progress /plots/*.plot -e "ssh -p ${REMOTE_PORT}" "${REMOTE_USER}"@"${REMOTE_HOST}":"${REMOTE_LOCATION}"
+			fi
 
-if [[ ${PLOTTER} == "madmax" ]]; then
-chia plotters madmax -k $SIZE -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -d /plots/
-elif [[ ${PLOTTER} == "blade" ]]; then
-apt-get install -y libgmp3-dev
-chia plotters bladebit -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -d /plots/
-else
-chia plots create -k $SIZE -n $COUNT -r $THREADS -b $MEMORY -c $CONTRACT -f $FARMERKEY -t $TMPDIR -2 $TMPDIR2 -d /plots/
-fi
+			if [[ ${PLOTTER} == "madmax" ]]; then
+				chia plotters madmax -k $SIZE -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -d $FINALDIR -u $BUCKETS
+			elif [[ ${PLOTTER} == "blade" ]]; then
+				apt-get install -y libgmp3-dev
+				chia plotters bladebit -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -d $FINALDIR
+			else
+				chia plotters madmax -k $SIZE -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -d $FINALDIR -u $BUCKETS
+			fi
 
+		else
 
-fi
+			if [[ ${PLOTTER} == "madmax" ]]; then
+				chia plotters madmax -k $SIZE -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -d $FINALDIR -u $BUCKETS
+			elif [[ ${PLOTTER} == "blade" ]]; then
+				apt-get install -y libgmp3-dev
+				chia plotters bladebit -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -d $FINALDIR
+			else
+				chia plotters madmax -k $SIZE -n $COUNT -r $THREADS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -d $FINALDIR -u $BUCKETS
+			fi
 
-done
+		fi
+
+		until (($(ls -la /plots/*.plot | wc -l) < 6)); do
+			echo "Deployment is full, please download and delete plots to make room for plotting! Sleeping for 60 seconds before checking for free space."
+			sleep 10
+		done
+
+	done
+
 fi
