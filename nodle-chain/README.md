33143c0b24176940fe3bb357d9103258bff74633
cryptoandcoffee <andrew@88plug.com>
2022-04-18 14:50:22 -0400

    Initial commit for Nodle.com node

---
+++ nodle-chain/README.md
@@ -0,0 +1,4 @@
+# Why use Akash?
+
+Welcome [Nodle](https://nodle.com/) node runners! You can easily run the latest release of a Nodle node on Akash.
+
---
+++ nodle-chain/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+apt-get update &&
+  apt-get upgrade -y &&
+  DEBIAN_FRONTEND=noninteractive apt-get install -y cmake pkg-config libssl-dev git clang build-essential curl netcat git &&
+  git clone https://github.com/NodleCode/chain &&
+  cd chain
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y &&
+  export PATH=$PATH:$HOME/.cargo/bin &&
+  scripts/init.sh &&
+  cargo build -p nodle-parachain --$PROFILE &&
+  cp /chain/target/$PROFILE/nodle-parachain /usr/local/bin
+
+mv /usr/share/ca* /tmp &&
+  rm -rf /usr/share/* &&
+  mv /tmp/ca-certificates /usr/share/ &&
+  rm -rf /usr/lib/python* &&
+  curl https://raw.githubusercontent.com/paritytech/polkadot/08c200f6540f67c16846d3152de50f0fbbc2a73d/node/service/res/rococo.json >/rococo.json
+
+exec nodle-parachain
