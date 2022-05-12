cc9366ac1b6b0c911ac84e58b69d02f96552fbde
cryptoandcoffee <andrew@88plug.com>
2022-05-12 19:14:30 -0400

    Cleanup

--- disk-speed-test/run.sh
+++ disk-speed-test/run.sh
@@ -3,4 +3,3 @@ echo "The first benchmark will test a small file..."
 dd if=/dev/zero of=/root/testfile bs=512 count=1000 oflag=direct
 echo "The second benchmark will test a large file..."
 dd if=/dev/zero of=/root/testfile bs=1G count=1 oflag=direct
-
