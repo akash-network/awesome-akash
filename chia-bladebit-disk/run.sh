99ac5c0f6cf07016360e28cf12a0626a71087661
Crypto and Coffee <null@cryptoandcoffee.com>
2022-08-12 14:42:53 -0700

    Use Ubuntu 22.04

--- chia-bladebit-disk/run.sh
+++ chia-bladebit-disk/run.sh
@@ -208,8 +208,6 @@ fi
 
 COUNT=0
 
-if [[ NODE == true ]]; then
-
 
 if [ ! -z $PLOTTER ]; then
   while :; do
@@ -258,7 +256,7 @@ if [ ! -z $PLOTTER ]; then
       elif [[ ${PLOTTER} == "madmax-ramdrive" ]]; then
         chia plotters madmax -k $SIZE -n $COUNT -r $CPU_UNITS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -2 /mnt/ram/ -d $FINALDIR -u $BUCKETS
       elif [[ ${PLOTTER} == "bladebit-disk" ]]; then
-        bladebit -t $CPU_UNITS -f $FARMERKEY -c $CONTRACT diskplot -t1 $TMPDIR --cache $RAMCACHE $FINALDIR
+        bladebit -t $CPU_UNITS -f $FARMERKEY -c $CONTRACT diskplot -b 64 -t1 $TMPDIR --cache $RAMCACHE -a $FINALDIR
 
         #                      You need about 192GiB(+|-) for high-frequency I/O Phase 1 calculations
         #                      to be completely in-memory.
@@ -277,7 +275,7 @@ if [ ! -z $PLOTTER ]; then
       elif [[ ${PLOTTER} == "bladebit" ]]; then
         chia plotters bladebit -n $COUNT -r $CPU_UNITS -c $CONTRACT -f $FARMERKEY -d $FINALDIR
       elif [[ ${PLOTTER} == "bladebit-disk" ]]; then
-        bladebit -t $CPU_UNITS -f $FARMERKEY -c $CONTRACT diskplot -t1 $TMPDIR --cache $RAMCACHE $FINALDIR
+        bladebit -t $CPU_UNITS -f $FARMERKEY -c $CONTRACT diskplot -b 64 -t1 $TMPDIR --cache $RAMCACHE -a $FINALDIR
       else
         chia plotters madmax -k $SIZE -n $COUNT -r $CPU_UNITS -c $CONTRACT -f $FARMERKEY -t $TMPDIR -d $FINALDIR -u $BUCKETS
       fi
