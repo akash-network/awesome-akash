cb50377c83950683655cb30facb6658319a94f9e
Andrew <andrew@cryptoandcoffee.com>
2022-09-16 00:20:39 -0700

    Run prysm from entrypoint

--- prysm-beacon/run.sh
+++ prysm-beacon/run.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 echo "Get latest version of prysm"
-curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output /prysm.sh && chmod +x /prysm.sh
+#curl https://raw.githubusercontent.com/prysmaticlabs/prysm/master/prysm.sh --output /prysm.sh && chmod +x /prysm.sh
+
+env CUSTOM_COMMAND=$CUSTOM_COMMAND
+env JWT_SECRET=$JWT_SECRET
 
 if [[ $JWT_SECRET != "" ]]; then
-echo $JWT_SECRET > jwt.hex
+echo $JWT_SECRET > /jwt.hex
+#echo "Showing secret for 3 seconds"
+#cat /root/.ethereum/jwt.hex
+#sleep 3
 else
 openssl rand -hex 32 | tr -d "\n" > "/jwt.hex"
-echo "Showing secret for 15 seconds"
-cat /jwt.hex
-sleep 15
+#echo "Showing secret for 15 seconds"
+#cat /root/.ethereum/jwt.hex
+#sleep 15
 fi
 
-echo "Auth ready"
+#echo "Auth ready"
+
 
 if [[ $CUSTOM_COMMAND != "" ]]; then
 $CUSTOM_COMMAND
