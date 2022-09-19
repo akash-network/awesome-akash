89ac7e718f18b80a9101d9d5f1d2e64a8df053ff
Andrew <andrew@cryptoandcoffee.com>
2022-09-19 08:01:22 -0700

    Add custom fee, interval, better defaults
    - Custom donation fee
    - Interval, BenchmarkInterval,MaxCrashes updated
    - Runmode set to standalone
    - Enable auto pool for other coins

--- rainbowminer/run.sh
+++ rainbowminer/run.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+sed -i 's/CUSTWALLET/"'"$CUSTOM_WALLET"'"/g' ./RainbowMiner/setup.json
 sed -i 's/BTC_WALLET/"'"$BTC_WALLET"'"/g' ./RainbowMiner/setup.json
 sed -i 's/XMR_WALLET/"'"$XMR_WALLET"'"/g' ./RainbowMiner/setup.json
 sed -i 's/DOGE_WALLET/"'"$DOGE_WALLET"'"/g' ./RainbowMiner/setup.json
 sed -i 's/RVN_WALLET/"'"$RVN_WALLET"'"/g' ./RainbowMiner/setup.json
 sed -i 's/XTZ_WALLET/"'"$XTZ_WALLET"'"/g' ./RainbowMiner/setup.json
+
 sed -i 's/AECURRENCY/"'"$AECurrency"'"/g' ./RainbowMiner/setup.json
 sed -i 's/REGION/"'"$REGION"'"/g' ./RainbowMiner/setup.json
+sed -i 's/DONATE/"'"$DONATE"'"/g' ./RainbowMiner/setup.json
 
 sed -i 's/SSLOPTION/"'"$SSL"'"/g' ./RainbowMiner/setup.json
 sed -i 's/MINER_NAME/"'"$MINER_NAME"'"/g' ./RainbowMiner/setup.json
