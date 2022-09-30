5c796882e29b8bb858588d0e8aa285c5ca38c7d1
Andrew <andrew@88plug.com>
2022-09-29 20:27:46 -0400

    Remove fail from curl

--- chia-bladebit/sync_rclone.sh
+++ chia-bladebit/sync_rclone.sh
@@ -7,7 +7,7 @@ rm /plots/failed.log
 #Run once to test upload
 
 for (( ; ; )); do
-  curl --connect-timeout 2 --retry 99 --retry-delay 2 --fail --retry-all-errors $JSON_SERVER > api_plots.log
+  curl -s --connect-timeout 2 --retry 99 --retry-all-errors --retry-delay 2 $JSON_SERVER > api_plots.log
   files=$(ls -la /plots/*.plot | awk '{print $9}')
   count=$(ls -la /plots/*.plot | wc -l)
 
@@ -38,7 +38,7 @@ for (( ; ; )); do
         #nohup rclone --contimeout 60s --timeout 300s --low-level-retries 10 --retries 99 -P --transfers=1 --fast-list --tpslimit=1 --bwlimit 100000000000000000000000 --dropbox-chunk-size=150M move $i $ENDPOINT_LOCATION:$ENDPOINT_DIR >>$i.log 2>&1 &
         echo $i >>/plots/pending.log
         START_TIME=$(date +%s)
-        curl --connect-timeout 2 --retry 99 --retry-delay 2 --fail --retry-all-errors --header "Content-Type: application/json" -d "filename=$i" -d "endpoint_location=$ENDPOINT_LOCATION" -d "endpoint_directory=$ENDPOINT_DIR" -d "start_time=$START_TIME" -d "provider=$AKASH_CLUSTER_PUBLIC_HOSTNAME" -X POST $JSON_SERVER >>$i.log
+        curl -s --connect-timeout 2 --retry 99 --retry-all-errors --retry-delay 2 -d "filename=$i" -d "endpoint_location=$ENDPOINT_LOCATION" -d "endpoint_directory=$ENDPOINT_DIR" -d "start_time=$START_TIME" -d "provider=$AKASH_CLUSTER_PUBLIC_HOSTNAME" -X POST $JSON_SERVER >>$i.log
       elif [[ $SHUFFLE_RCLONE_ENDPOINT == true ]]; then
         #Uses same directory name
         ENDPOINT_LOCATION=$(cat /root/.config/rclone/rclone.conf | grep "\[" | sort | uniq | shuf | tail -n1 | sed 's/[][]//g')
@@ -63,7 +63,7 @@ for (( ; ; )); do
         nohup rclone --retries 99 --contimeout 60s --timeout 300s --low-level-retries 10 --retries 99 --dropbox-chunk-size 150M --drive-chunk-size 256M --progress move $i $ENDPOINT_LOCATION:/$ENDPOINT_DIR >>$i.log 2>&1 &
         START_TIME=$(date +%s)
         if [[ $JSON_SERVER != "" ]]; then
-          curl --retry 99 --retry-all-errors --fail --header "Content-Type: application/json" -d "filename=$i" -d "endpoint_location=$ENDPOINT_LOCATION" -d "endpoint_directory=$ENDPOINT_DIR" -d "start_time=$START_TIME" -d "provider=$AKASH_CLUSTER_PUBLIC_HOSTNAME" -X POST $JSON_SERVER >>$i.log
+          curl --connect-timeout 2 --retry 99 --retry-delay 2 -d "filename=$i" -d "endpoint_location=$ENDPOINT_LOCATION" -d "endpoint_directory=$ENDPOINT_DIR" -d "start_time=$START_TIME" -d "provider=$AKASH_CLUSTER_PUBLIC_HOSTNAME" -X POST $JSON_SERVER >>$i.log
         fi
       fi
 
@@ -92,14 +92,14 @@ for (( ; ; )); do
 
         if [[ $result != "upload_complete" && $progress == "100" ]]; then
         END=$(date +%s)
-        curl --connect-timeout 2 --retry 99 --retry-delay 2 --fail --retry-all-errors --header "Content-Type: application/json" -d "progress=upload_complete" -d "finish_time=$END" -X PATCH $JSON_SERVER/$id
+        curl --connect-timeout 2 --retry 99 --retry-all-errors --retry-delay 2 -d "progress=upload_complete" -d "finish_time=$END" -X PATCH $JSON_SERVER/$id
         rm $i.log
         sed -i "s|$i||g" /plots/pending.log
         sed -i '/^$/d;s/[[:blank:]]//g' /plots/pending.log
         fi
 
       else
-        curl --connect-timeout 2 --retry 99 --retry-delay 2 --fail --retry-all-errors --header "Content-Type: application/json" -d "total_time=$TOTAL_TIME" -d "progress=$progress" -d "speed=$speed" -X PATCH $JSON_SERVER/$id
+        curl --connect-timeout 2 --retry 99 --retry-all-errors --retry-delay 2 -d "total_time=$TOTAL_TIME" -d "progress=$progress" -d "speed=$speed" -X PATCH $JSON_SERVER/$id
       fi
 
     else
@@ -107,8 +107,9 @@ for (( ; ; )); do
        result=$(cat api_plots.log | jq -r '.[] | select(.filename == "'"$i"'").progress')
        if [[ $result != "Possible error detected in logs" ]]; then
        echo "Updating the API with the error"
-       error=$(cat $i.log | grep ERROR | head -n1)
-       curl --connect-timeout 2 --retry 99 --retry-delay 2 --fail --retry-all-errors --header "Content-Type: application/json" -d "error=$error" -X PATCH $JSON_SERVER/$id
+       #error=$(cat $i.log | grep ERROR | head -n1)
+       error="ERROR"
+       curl --connect-timeout 2 --retry 99 --retry-all-errors --retry-delay 2 -d "error=$error" -X PATCH $JSON_SERVER/$id
        fi
     fi
   done
