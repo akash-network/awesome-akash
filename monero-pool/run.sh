4d41a779b71a79ccad24a255010ee6905a0d547e
88plug <andrew@88plug.com>
2022-10-16 12:56:20 -0700

    ZMQ Notify on

--- monero-pool/run.sh
+++ monero-pool/run.sh
@@ -5,6 +5,8 @@ DATA_WALLET=/data/wallet
 DATA_WALLET_LOG=/data/wallet.log
 DATA_POOL=/data/pool
 mkdir -p $DATA_MONERO
+mkdir -p $DATA_WALLET
+mkdir -p $DATA_POOL
 
 
 if [[ $FAST_SYNC == true && $LOCAL == false ]]; then
@@ -30,20 +32,12 @@ sed -i "s/pool-fee-wallet =\(.*\)/pool-fee-wallet = $FEE_WALLET/"            /us
 sed -i "s/pool-fee =\(.*\)/pool-fee = $POOL_FEE/"                            /usr/local/etc/pool.conf
 sed -i "s/payment-threshold =\(.*\)/payment-threshold = $PAYMENT_THRESHOLD/" /usr/local/etc/pool.conf
 
-if [[ $LOCAL == true ]]; then
+#if [[ $LOCAL == true ]]; then #Testing
 sed -i "s/block-notified =\(.*\)/block-notified = 1/"                        /usr/local/etc/pool.conf
-fi
+#fi
 
 sed -i "s/data-dir =\(.*\)/data-dir = \/data\/pool/"                         /usr/local/etc/pool.conf
 sed -i "s/log-file =\(.*\)/log-file = \/data\/pool.log/"                     /usr/local/etc/pool.conf
-
-
-#db-sync-mode=safe
-#enforce-dns-checkpointing=1
-#out-peers=64              # This will enable much faster sync and tx awareness; the default 8 is suboptimal nowadays
-#in-peers=1024             # The default is unlimited; we prefer to put a cap on this
-#limit-rate-up=1048576     # 1048576 kB/s == 1GB/s; a raise from default 2048 kB/s; contribute more to p2p network
-#limit-rate-down=1048576   # 1048576 kB/s == 1GB/s; a raise from default 8192 kB/s; allow for faster initial sync
 
 echo Running Monero daemon...
 
@@ -56,6 +50,7 @@ if [[ $LOCAL == true ]]; then
 else
 /usr/local/bin/monerod --detach --data-dir $DATA_MONERO \
     --rpc-bind-ip 127.0.0.1 --rpc-bind-port 28081 \
+    --block-notify '/usr/bin/pkill -USR1 notify' \ #Testing this command
     --rpc-restricted-bind-ip 0.0.0.0 --rpc-restricted-bind-port 28092 --confirm-external-bind $MONERO_ARGS \
     --db-sync-mode safe --enforce-dns-checkpointing --out-peers 128 --in-peers 1024 --limit-rate-up 1048576 --limit-rate-down 1048576
 fi
@@ -64,7 +59,6 @@ echo Sleeping for 30 secs...
 sleep 30
 
 echo Running Monero wallet RPC daemon...
-mkdir -p $DATA_WALLET
 
 /usr/local/bin/monero-wallet-rpc --detach --wallet-dir $DATA_WALLET \
     --log-file $DATA_WALLET_LOG --max-log-file-size 10485000 --max-log-files 10 \
@@ -83,10 +77,4 @@ sleep 30
 
 
 echo Running Monero pool...
-mkdir -p $DATA_POOL
-#/usr/local/bin/monero-pool --config-file /usr/local/etc/pool.conf $@
 /usr/local/bin/monero-pool --config-file /usr/local/etc/pool.conf
-
-
-#monerod --p2p-bind-ip=0.0.0.0 --p2p-bind-port=18080 --rpc-bind-ip=0.0.0.0 --rpc-bind-port=18081 --non-interactive --confirm-external-bind
-
