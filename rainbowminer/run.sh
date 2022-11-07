a60852a1c61c1aaa55f874f608aea7763efb153c
88plug <andrew@88plug.com>
2022-11-07 08:35:25 -0800

    Major update for threads fix

--- rainbowminer/run.sh
+++ rainbowminer/run.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+
+echo "Checking CPU in pod"
+if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ]; then
+CPU_COUNT=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
+else
+CPU_COUNT=$(cat /sys/fs/cgroup/cpu.max | awk '{print $1}')
+fi
+CPU_COUNT=$(echo "scale=0; $CPU_COUNT/100000" | bc -l) #Convert to Cores
+echo "Found $CPU_COUNT cpus available."
+
+CPUS=$CPU_COUNT
+
 sed -i 's/CUSTWALLET/"'"$CUSTOM_WALLET"'"/g' ./RainbowMiner/setup.json
 sed -i 's/BTC_WALLET/"'"$BTC_WALLET"'"/g' ./RainbowMiner/setup.json
 sed -i 's/XMR_WALLET/"'"$XMR_WALLET"'"/g' ./RainbowMiner/setup.json
@@ -19,5 +31,14 @@ sed -i 's/EXCLUDE_POOL_NAME/"'"$EXCLUDE_POOL_NAME"'"/g' ./RainbowMiner/setup.jso
 sed -i 's/EXCLUDE_COIN/"'"$EXCLUDE_COIN"'"/g' ./RainbowMiner/setup.json
 sed -i 's/EXCLUDE_MINER_NAME/"'"$EXCLUDE_MINER_NAME"'"/g' ./RainbowMiner/setup.json
 sed -i 's/POOLS/"'"$POOLS"'"/g' ./RainbowMiner/setup.json
+sed -i 's/INTERVAL/"'"$BENCHMARK_INTERVAL"'"/g' ./RainbowMiner/setup.json
+sed -i 's/CPUS/"'"$CPUS"'"/g' ./RainbowMiner/setup.json
+sed -i 's/PROHASHING/"'"$PROHASHING"'"/g' ./RainbowMiner/setup.json
+sed -i 's/MINING_DUTCH/"'"$MINING_DUTCH"'"/g' ./RainbowMiner/setup.json
+
+
+
+#remove affinity and threads from miners
+
 
 ./RainbowMiner/start.sh
