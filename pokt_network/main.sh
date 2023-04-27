729b4fa25c9fc3b92fa4b40161178d2754a1638a
Dimokus <Dimokus@mail.ru>
2023-04-28 00:24:09 +0300

    Update main.sh

--- pokt_network/main.sh
+++ pokt_network/main.sh
@@ -54,9 +54,7 @@ if [[ -n $LINK_SNAPSHOT ]]
 then
 mkdir -p $HOME/.pocket/data
 SIZE=`wget --spider $LINK_SNAPSHOT 2>&1 | awk '/Length/ {print $2/(1024*1024*1024)}' | cut -d '.' -f 1`
-wget -qO- $LINK_SNAPSHOT | pv -bep -s "$SIZE"G > /root/latest.tar.gz
-tar -xz /root/latest.tar.gz -C $HOME/.pocket/data
-rm /root/latest.tar.gz
+wget -qO- $LINK_SNAPSHOT | pv -bep -s "$SIZE"G -i 5 | tar -xz -C $HOME/.pocket/data
 fi
 echo === Run node ===
 mkdir -p /root/pocket/log    
