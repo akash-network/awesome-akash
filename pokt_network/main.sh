3b71eaf18e69f2811c9c90f79fc4603982f82366
Dimokus <Dimokus@mail.ru>
2023-04-28 01:15:13 +0300

    Update main.sh

--- pokt_network/main.sh
+++ pokt_network/main.sh
@@ -58,7 +58,7 @@ export LINK_SNAPSHOT=$LINK_SNAPSHOT
 mkdir -p $HOME/.pocket/data
 SIZE=`wget --spider $LINK_SNAPSHOT 2>&1 | awk '/Length/ {print $2}'
 echo $SIZE $LINK_SNAPSHOT
-(wget -qO- --progress=bar:force $LINK_SNAPSHOT | pv -bep -s $SIZE -i 5 | tar -xz -C $HOME/.pocket/data) 2>&1 | stdbuf -o0 tr '\r' '\n'
+(wget -nv -O - $LINK_SNAPSHOT | pv -petrafb -s $SIZE -i 5 | tar -xz -C $HOME/.pocket/data) 2>&1 | stdbuf -o0 tr '\r' '\n'
 fi
 echo === Run node ===
 mkdir -p /root/pocket/log    
