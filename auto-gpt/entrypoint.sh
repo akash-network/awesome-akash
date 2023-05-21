faca33bd6519a5484f92448a0a2b3d61be2a9e48
88plug <andrew@88plug.com>
2023-05-21 13:40:41 -0700

    auto-gpt always up-to-date

---
+++ auto-gpt/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Upgrade pip
+pip install --upgrade pip
+
+# Clone the Auto-GPT repository
+git clone https://github.com/Significant-Gravitas/Auto-GPT.git /app
+
+# Set the working directory
+cd /app
+
+pip install --no-cache-dir -r requirements.txt
+
+gotty -w --random-url-length 16 python -m autogpt
