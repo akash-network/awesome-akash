fd930b719c9b416a51e3baee05a9a222d939654d
Dimokus <Dimokus@mail.ru>
2023-05-29 21:15:50 +0300

    Update entrypoint.sh

--- AUTOMATIC1111/entrypoint.sh
+++ AUTOMATIC1111/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
-git clone https://github.com/Dimokus88/stable-diffusion-webui.git
-if [ -n $MODEL_LINK ]
-then
-pip install diffusers transformers accelerate scipy safetensors
-wget -P /stable-diffusion-webui/models/Stable-diffusion "$MODEL_LINK"
-fi
-/stable-diffusion-webui/webui.sh "$@"
+wget -P /stable-diffusion-webui/models/Stable-diffusion https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt
+/stable-diffusion-webui/webui.sh --update-check --xformers --listen --port 8080 --medvram
