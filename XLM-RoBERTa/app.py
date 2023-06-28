9b81491c550e9ff5d3e7d75e43cbaa1a1787b6a7
Clyde Carver <80094928+clydedevv@users.noreply.github.com>
2023-06-28 14:23:07 +0200

    Updated app.py to interact with XLM-RoBERTa"

--- XLM-RoBERTa/app.py
+++ XLM-RoBERTa/app.py
@@ -1,16 +1,29 @@
 from flask import Flask, request, jsonify
-from transformers import pipeline
+from transformers import XLMRobertaModel, XLMRobertaTokenizer
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.get_json(force=True)
-    model_name = "xlm-roberta-base"
-    unmasker = pipeline('fill-mask', model=model_name)
-    prediction = unmasker(data['input'])
-    return jsonify(prediction)
+# Load pre-trained model and tokenizer
+model = XLMRobertaModel.from_pretrained('xlm-roberta-base')
+tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
+
+@app.route('/encode', methods=['POST'])
+def encode():
+    # Get the text from the request
+    text = request.json.get('text')
+
+    # Check if text is not empty
+    if not text:
+        return jsonify({'error': 'No text provided'}), 400
+
+    # Encode the text
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+
+    # Convert the output to a list so it can be JSON serialized
+    output_list = output[0].tolist()
+
+    return jsonify({'encoded_representation': output_list})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
-
