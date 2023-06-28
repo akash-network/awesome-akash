32f5a839df64b05297b68393cabd06adb4f3211c
clydedevv <clydecarver@gmail.com>
2023-06-28 13:17:24 +0000

    Updated app.py to interact with XLM-RoBERTa to handle /encode

--- XLM-RoBERTa/app.py
+++ XLM-RoBERTa/app.py
@@ -1,29 +1,21 @@
 from flask import Flask, request, jsonify
 from transformers import XLMRobertaModel, XLMRobertaTokenizer
+import torch
 
 app = Flask(__name__)
 
-# Load pre-trained model and tokenizer
-model = XLMRobertaModel.from_pretrained('xlm-roberta-base')
-tokenizer = XLMRobertaTokenizer.from_pretrained('xlm-roberta-base')
+MODEL_NAME = 'xlm-roberta-base'
+tokenizer = XLMRobertaTokenizer.from_pretrained(MODEL_NAME)
+model = XLMRobertaModel.from_pretrained(MODEL_NAME)
 
 @app.route('/encode', methods=['POST'])
 def encode():
-    # Get the text from the request
-    text = request.json.get('text')
+    text = request.json['text']
+    inputs = tokenizer(text, return_tensors='pt')
+    outputs = model(**inputs)
 
-    # Check if text is not empty
-    if not text:
-        return jsonify({'error': 'No text provided'}), 400
-
-    # Encode the text
-    encoded_input = tokenizer(text, return_tensors='pt')
-    output = model(**encoded_input)
-
-    # Convert the output to a list so it can be JSON serialized
-    output_list = output[0].tolist()
-
-    return jsonify({'encoded_representation': output_list})
+    # Convert tensor to list and return as JSON
+    return jsonify(outputs.last_hidden_state.tolist())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
