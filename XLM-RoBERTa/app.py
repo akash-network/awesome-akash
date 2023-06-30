ba44f4a463d9063f31c5b19412e06ead0a8822fa
clydedevv <clydecarver@gmail.com>
2023-06-30 15:06:55 +0000

    updating flask app to be more interactive

--- XLM-RoBERTa/app.py
+++ XLM-RoBERTa/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from transformers import XLMRobertaTokenizer, XLMRobertaModel
 
 app = Flask(__name__)
@@ -7,14 +7,15 @@ MODEL_NAME = 'xlm-roberta-base'
 tokenizer = XLMRobertaTokenizer.from_pretrained(MODEL_NAME)
 model = XLMRobertaModel.from_pretrained(MODEL_NAME)
 
-@app.route('/encode', methods=['POST'])
-def encode():
-    data = request.get_json()
-    text = data.get('text')
-    encoded_input = tokenizer(text, return_tensors='pt')
-    return jsonify(encoded_input)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text = request.form['text']
+        encoded_input = tokenizer(text, return_tensors='pt')
+        output = model(**encoded_input)
+        return render_template('index.html', output=output)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     print("XLMRoberta deployed successfully!")
     app.run(host='0.0.0.0', port=80)
-
