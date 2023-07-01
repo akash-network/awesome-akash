31218245a580a7ba793c4236b5fbc8471049fe8b
clydedevv <clydecarver@gmail.com>
2023-07-01 15:25:08 +0000

    added sentiment analysis to flask app

--- XLM-RoBERTa/app.py
+++ XLM-RoBERTa/app.py
@@ -1,21 +1,18 @@
 from flask import Flask, request, jsonify, render_template
-from transformers import XLMRobertaTokenizer, XLMRobertaModel
+from transformers import pipeline
 
 app = Flask(__name__)
 
-MODEL_NAME = 'xlm-roberta-base'
-tokenizer = XLMRobertaTokenizer.from_pretrained(MODEL_NAME)
-model = XLMRobertaModel.from_pretrained(MODEL_NAME)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        text = request.form['text']
-        encoded_input = tokenizer(text, return_tensors='pt')
-        output = model(**encoded_input)
-        return render_template('index.html', output=output)
+@app.route('/')
+def home():
     return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.form.get('text')
+    nlp = pipeline("sentiment-analysis", model="cardiffnlp/twitter-xlm-roberta-base-sentiment")
+    result = nlp(text)[0]
+    return jsonify(result)
+
 if __name__ == '__main__':
-    print("XLMRoberta deployed successfully!")
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
