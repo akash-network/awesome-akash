a828b60b1935b13c7f2c097d60020950695ffe40
88plug <andrew@88plug.com>
2023-09-07 14:37:44 -0700

    xmrig finalized

--- xmrig-c11/run.sh
+++ xmrig-c11/run.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Checking CPU in pod"
-if [ -f /sys/fs/cgroup/cpu/cpu.cfs_quota_us ]; then
-CPU_COUNT=$(cat /sys/fs/cgroup/cpu/cpu.cfs_quota_us)
-else
-CPU_COUNT=$(cat /sys/fs/cgroup/cpu.max | awk '{print $1}')
-fi
-CPU_COUNT=$(echo "scale=0; $CPU_COUNT/100000" | bc -l) #Convert to Cores
-echo "Found $CPU_COUNT cpus available."
+remove_quotes() {
+  local str="$1"
+  str="${str%\"}"
+  str="${str#\"}"
+  echo "$str"
+}
 
-echo "Checking Memory in pod"
-if [ -f /sys/fs/cgroup/memory/memory.limit_in_bytes ]; then
-MEMORY_SIZE=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
-else
-MEMORY_SIZE=$(cat /sys/fs/cgroup/memory.max)
-fi
-MEMORY_SIZE=$(echo "scale=2; $MEMORY_SIZE/1024/1024/1024" | bc -l) #Convert to Gi
-
-echo "Found $MEMORY_SIZE of memory available."
+ALGO=$(remove_quotes "$ALGO")
+PASSWORD=$(remove_quotes "$PASSWORD")
+POOL=$(remove_quotes "$POOL")
+WALLET_ADDRESS=$(remove_quotes "$WALLET_ADDRESS")
+OPTIONS=$(remove_quotes "$OPTIONS")
 
 if [ -z $WALLET ]; then
     echo "Please examine the SDL and be sure to set your Monero Wallet Address in the WALLET= variable."
@@ -25,58 +19,15 @@ if [ -z $WALLET ]; then
     exit
 fi
 
-echo "Checking for NUMA nodes"
-if [[ $(lscpu | grep "NUMA" | head -n1 | awk '{print $3}') > 1 ]]; then
-
-    echo "Got more than 1 NUMA node on this provider!  Ensuring you have set enough memory..."
-
-        NUMA_MIN_MEMORY=4.75
-        #4416Mi
-
-        if (($(echo "$MEMORY_SIZE >= $NUMA_MIN_MEMORY" | bc -l))); then
-            echo "Found enough memory!"
-        else
-            echo "This provider has $(lscpu | grep "NUMA" | head -n1 | awk '{print $3}') NUMA nodes"
-            echo "Increase the requested memory for this deployment to >= 4.75Gi"
-            echo "You must close this deployment to change the memory requested."
-            echo "------------------------------------------"
-            echo "Deployment will continue in SLOW mode after 30 seconds, setting RANDOMX_MODE=light."
-            sleep 30
-            RANDOMX_MODE=light
-        fi
-
-
-else
-
-    echo "Could not detect more than 1 NUMA node, assuming single NUMA and lowering memory limits."
-
-        NUMA_MIN_MEMORY=3
-        #4416Mi
-
-        if (($(echo "$MEMORY_SIZE >= $NUMA_MIN_MEMORY" | bc -l))); then
-            echo "Found enough memory!"
-        else
-            echo "This provider has $(lscpu | grep "NUMA" | head -n1 | awk '{print $3}') NUMA nodes"
-            echo "Increase the requested memory for this deployment to >= 3Gi"
-            echo "You must close this deployment to change the memory requested."
-            echo "If you don't want to close this deployment, please switch to RANDOMX_MODE=light and update the deployment."
-            echo "------------------------------------------"
-            echo "Deployment will continue in SLOW mode after 30 seconds, setting RANDOMX_MODE=light."
-            sleep 30
-            RANDOMX_MODE=light
-        fi
-fi
 
 WORKER=$(echo ${WORKER}-${AKASH_CLUSTER_PUBLIC_HOSTNAME})
 
-cd /xmrig
 echo "Using POOL: ${POOL} WALLET: ${WALLET}  WORKER: ${WORKER} TLS: ${TLS} TLS_FINGERPRINT: ${TLS_FINGERPRINT}"
-CUSTOM_OPTIONS=$(sed -e 's/^"//' -e 's/"$//' <<<"$CUSTOM_OPTIONS") #Remove quotes
 
 if [[ ${TLS_FINGERPRINT} != "" && ${TLS} == "true" ]]; then
-    ./xmrig -a ${ALGO} --url ${POOL} --user ${WALLET} --rig-id ${WORKER} --pass ${WORKER} --tls --tls-fingerprint ${TLS_FINGERPRINT} --http-host 0.0.0.0 --http-port 8080 --syslog --no-color --verbose --cuda --no-cpu $CUSTOM_OPTIONS
+    ./xmrig/xmrig -a ${ALGO} --url ${POOL} --user ${WALLET} --rig-id ${WORKER} --pass ${WORKER} --tls --tls-fingerprint ${TLS_FINGERPRINT} --http-host 0.0.0.0 --http-port 8080 --syslog --no-color --verbose --cuda --no-cpu $CUSTOM_OPTIONS
 elif [[ ${TLS_FINGERPRINT} == "" && ${TLS} == "true" ]]; then
-    ./xmrig -a ${ALGO} --url ${POOL} --user ${WALLET} --rig-id ${WORKER} --pass ${WORKER} --tls --http-host 0.0.0.0 --http-port 8080 --syslog --no-color --verbose --cuda --no-cpu $CUSTOM_OPTIONS
+    ./xmrig/xmrig -a ${ALGO} --url ${POOL} --user ${WALLET} --rig-id ${WORKER} --pass ${WORKER} --tls --http-host 0.0.0.0 --http-port 8080 --syslog --no-color --verbose --cuda --no-cpu $CUSTOM_OPTIONS
 else
-    ./xmrig -a ${ALGO} --url ${POOL} --user ${WALLET} --rig-id ${WORKER} --pass ${WORKER} --http-host 0.0.0.0 --http-port 8080 --syslog --no-color --verbose --cuda --no-cpu $CUSTOM_OPTIONS
+    ./xmrig/xmrig -a ${ALGO} --url ${POOL} --user ${WALLET} --rig-id ${WORKER} --pass ${WORKER} --http-host 0.0.0.0 --http-port 8080 --syslog --no-color --verbose --cuda --no-cpu $CUSTOM_OPTIONS
 fi
