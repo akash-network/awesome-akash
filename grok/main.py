915207ea85d82f91978b336dfeb9d7c077f3da87
Yusuf Praditya <47532266+yusufpraditya@users.noreply.github.com>
2024-03-29 09:04:08 +0700

    add loading animation

--- grok/main.py
+++ grok/main.py
@@ -4,21 +4,52 @@ from sentencepiece import SentencePieceProcessor
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from huggingface_hub import snapshot_download
+import os
+import time
+import logging
 
-print("After grok model downloaded, it will take 5-10 minutes to load checkpoints.")
+logging.basicConfig(level=logging.INFO, filename="grok.log",filemode="w", format="%(asctime)s %(levelname)s %(message)s")
+
+MAX_NEW_TOKENS = int(os.environ.get("MAX_NEW_TOKENS", 100))
+MODEL_PATH = "./grok-model"
+
+is_downloaded = False
+
+while not is_downloaded:
+  try:
+    snapshot_download(repo_id="hpcai-tech/grok-1", local_dir=MODEL_PATH, local_dir_use_symlinks=False, resume_download=True, max_workers=4, revision="d34f045119ab9a385517721dbdb40ba2036a5d60")
+
+    bin_files = [file for file in os.listdir(MODEL_PATH) if file.lower().endswith('.bin')]
+
+    if len(bin_files) == 65:
+      is_downloaded = True
+      print("Download finished. Checkpoints will be loaded and takes about 10 minutes.")
+      print("If after 10 minutes it still looks 'stuck', try reloading Cloudmos.")
+      logging.info("Download finished.")
+  except Exception as error:
+    logging.error(error)
+    print(error)
+    print("Retrying..")
+    time.sleep(5)
 
 torch.set_default_dtype(torch.bfloat16)
+
+logging.info("Loading model..")
+
 model = AutoModelForCausalLM.from_pretrained(
-    "hpcai-tech/grok-1",
+    MODEL_PATH,
     trust_remote_code=True,
     device_map="auto",
     torch_dtype=torch.bfloat16,
+    local_files_only=True
 )
+
+logging.info("Model loaded.")
+
 sp = SentencePieceProcessor(model_file="tokenizer.model")
 
 app = FastAPI()
-
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 @app.get("/infer")
 def inference(input):
@@ -28,7 +59,7 @@ def inference(input):
     attention_mask = torch.ones_like(input_ids)
 
     generate_kwargs = {
-      "max_new_tokens": 64
+      "max_new_tokens": MAX_NEW_TOKENS
     }
 
     inputs = {
@@ -43,6 +74,8 @@ def inference(input):
     return { "output": decoded_output }
   except:
     return { "output": "Error. Try again!" }
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 @app.get("/")
 def index() -> FileResponse:
