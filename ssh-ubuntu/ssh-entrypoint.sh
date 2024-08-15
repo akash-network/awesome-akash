5447e9fae0be625523974a6c58c90a8b9c584f93
Maxime Beauchamp <maximebeauchamp7@gmail.com>
2024-08-15 16:15:57 -0400

    fix: update docker files with latest changes

--- ssh-ubuntu/ssh-entrypoint.sh
+++ ssh-ubuntu/ssh-entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "$SSH_PUBKEY" | tee ~/.ssh/authorized_keys; \
+if [ -z "$SSH_PUBKEY" ]; then
+  echo "$(date): Error: SSH_PUBKEY is not set"
+else
+  echo "$SSH_PUBKEY" > ~/.ssh/authorized_keys
+  pubkey_preview="${SSH_PUBKEY:0:20}***"
+  echo "$(date): SSH_PUBKEY (${pubkey_preview}) written to ~/.ssh/authorized_keys"
+fi
+
 /usr/sbin/sshd
 
 exec "$@"
+
